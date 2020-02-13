7423504ede22922812575aa28d4efc2ce02a4191
Matthew O'Connor <Matthew@mbp>
2020-02-13 20:44:41 +0000

    'loader added'

--- assets/js/esquire.js
+++ assets/js/esquire.js
@@ -20,6 +20,18 @@ var burger_menu;
 
 var scroll_distance = 500;
 
+
+$('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+$(window).on('load', function(){
+  setTimeout(removeLoader, 1000); //wait for page load PLUS two seconds.
+});
+function removeLoader(){
+    $( "#loadingDiv" ).fadeOut(500, function() {
+      // fadeOut complete. Remove the loading div
+      $( "#loadingDiv" ).remove(); //makes page more lightweight 
+  });  
+}
+
 $(document).ready(function () {
     BrowserDetect.init();
 
